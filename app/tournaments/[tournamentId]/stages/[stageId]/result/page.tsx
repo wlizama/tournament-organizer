54afe55308eaf19e12e9f56782d4b1fd5f9ea04b
bouahth2018 <bouahth2018@gmail.com>
2023-08-02 10:09:59 +0700

    Update bracket

--- app/tournaments/[tournamentId]/stages/[stageId]/result/page.tsx
+++ app/tournaments/[tournamentId]/stages/[stageId]/result/page.tsx
@@ -13,16 +13,47 @@ async function getStage(stageId: string) {
   return stage;
 }
 
+async function getParticipants(id: string) {
+  return await prisma.participant.findMany({
+    where: {
+      tournamentId: id,
+    },
+  });
+}
+
 interface Params {
   params: {
-    // tournamentId: string;
+    tournamentId: string;
     stageId: string;
   };
 }
 
 export default async function StageResult({ params }: Params) {
-  const stage = await getStage(params.stageId);
-  const bracket = createBracket(stage);
+  const stageData = getStage(params.stageId);
+  const participantsData = getParticipants(params.tournamentId);
+  const [stage, participants] = await Promise.all([
+    stageData,
+    participantsData,
+  ]);
+  const teams = [
+    "Team 1",
+    "Team 2",
+    "Team 3",
+    "Team 4",
+    "Team 5",
+    "Team 6",
+    "Team 7",
+    "Team 8",
+    "Team 9",
+    "Team 10",
+    "Team 11",
+    "Team 12",
+    "Team 13",
+    "Team 14",
+    "Team 15",
+    "Team 16",
+  ];
+  const bracket = createBracket(stage, teams);
   const allMatches = bracket.reduce(
     (matches, round) => [...matches, ...round],
     []
@@ -30,34 +61,46 @@ export default async function StageResult({ params }: Params) {
   const links = createLinks(bracket);
 
   return (
-    <div
-      className="relative flex flex-col bg-white text-left overflow-hidden rounded m-0"
-      id="card"
-    >
-      <div className="p-5 border-b" id="card-header">
-        <h2 className="text-2xl font-medium">{stageTypes[stage!.type].name}</h2>
-      </div>
-      <div className="flex-1 p-5 overflow-hidden break-words" id="card-content">
-        {/* CARD */}
-        <div className="block overflow-x-auto break-words">
-          <div className="flex flex-wrap box-border min-w-max">
-            {bracket.map((round, i) => (
-              <div key={`round-${i}`} className="box-border min-w-0 mr-8">
-                <div className="min-w-[12rem] p-3 bg-neutral-100 rounded">
-                  <h2 className="text-sm font-bold text-center">
-                    Round {i + 1}
-                  </h2>
+    <div className="px-4 sm:px-6 lg:px-8">
+      <h1 className="text-3xl font-medium">{stage!.name}</h1>
+      <div
+        className="relative flex flex-col bg-white text-left overflow-hidden rounded m-0 mt-8"
+        id="card"
+      >
+        <div className="p-5 border-b" id="card-header">
+          <h2 className="text-2xl font-medium">
+            {stageTypes[stage!.type].name}
+          </h2>
+        </div>
+        <div
+          className="flex-1 p-5 overflow-hidden break-words"
+          id="card-content"
+        >
+          {/* CARD */}
+          <div className="block overflow-x-auto break-words">
+            <div className="flex flex-wrap box-border min-w-max">
+              {bracket.map((round, i) => (
+                <div key={`round-${i}`} className="box-border min-w-0 mr-8">
+                  <div className="min-w-[12rem] p-3 bg-neutral-100 rounded">
+                    <h2 className="text-sm font-bold text-center">
+                      Round {i + 1}
+                    </h2>
+                  </div>
                 </div>
-              </div>
-            ))}
-          </div>
-          {/* <div className="flex flex-wrap box-border w-[56rem]">
-            {bracket.map((round, i) => (
-              <div key={`round-${i}`} className="box-border min-w-0 mr-8">
-                {round.map((match, j) => (
+              ))}
+            </div>
+            <div
+              className="relative block"
+              style={{
+                width: bracket.length * 14 + "rem",
+                height: bracket[0].length * 5 + "rem",
+              }}
+            >
+              <div className="absolute z-[1] block" id="bracket-nodes">
+                {allMatches.map((match, i) => (
                   <div
-                    key={`match-${j}`}
-                    className="flex items-center"
+                    key={`match-${i}`}
+                    className="box-border min-w-0 mr-8 absolute"
                     style={{
                       left: match.left,
                       top: match.top,
@@ -65,74 +108,62 @@ export default async function StageResult({ params }: Params) {
                       height: "3.875rem",
                     }}
                   >
-                    Match {j + 1} ({match.name})
+                    <div className="flex relative box-border min-w-[12rem] p-2 rounded border border-neutral-300">
+                      <div
+                        className="flex absolute -top-2 left-2 right-2 z-[1] text-xs"
+                        id="header"
+                      >
+                        <div className="text-ellipsis overflow-hidden whitespace-nowrap bg-white px-1 mb-2 text-neutral-500">
+                          {match.name}
+                        </div>
+                      </div>
+                      <div
+                        className="flex-[3_1_0%] block text-neutral-300 text-sm"
+                        id="record"
+                      >
+                        <div className="flex items-center mb-[1px]">
+                          <div className="text-neutral-900">
+                            {match.match[0] ? (
+                              match.match[0]
+                            ) : (
+                              <span>&nbsp;</span>
+                            )}
+                          </div>
+                        </div>
+                        <div className="flex items-center mb-0">
+                          <div className="text-neutral-900">
+                            {match.match[1] ? (
+                              match.match[1]
+                            ) : (
+                              <span>&nbsp;</span>
+                            )}
+                          </div>
+                        </div>
+                      </div>
+                      <div className="" id="state disabled"></div>
+                    </div>
                   </div>
                 ))}
               </div>
-            ))}
-          </div> */}
-          <div
-            className="relative block"
-            style={{
-              width: bracket.length * 14 + "rem",
-              height: bracket[0].length * 5 + "rem",
-            }}
-          >
-            <div className="absolute z-[1] block" id="bracket-nodes">
-              {allMatches.map((match, i) => (
-                <div
-                  key={`match-${i}`}
-                  className="box-border min-w-0 mr-8 absolute"
-                  style={{
-                    left: match.left,
-                    top: match.top,
-                    width: "12rem",
-                    height: "3.875rem",
-                  }}
-                >
-                  <div className="flex relative box-border min-w-[12rem] p-2 rounded border border-neutral-300">
-                    <div
-                      className="flex absolute -top-2 left-2 right-2 z-[1] text-xs"
-                      id="header"
-                    >
-                      <div className="text-ellipsis overflow-hidden whitespace-nowrap bg-white px-1 mb-2 text-neutral-500">
-                        {match.name}
-                      </div>
-                    </div>
-                    <div
-                      className="flex-[3_1_0%] block text-neutral-300 text-sm"
-                      id="record"
-                    >
-                      <div className="flex items-center mb-[1px]">
-                        <div className="">Team A</div>
-                      </div>
-                      <div className="flex items-center mb-0">
-                        <div className="">Team B</div>
-                      </div>
-                    </div>
-                    <div className="" id="state disabled"></div>
-                  </div>
-                </div>
-              ))}
+              <svg
+                className="absolute z-0 overflow-hidden w-full h-full"
+                x={0}
+                y={0}
+                viewBox={`0 0 ${bracket.length * 14000} ${
+                  bracket[0].length * 5000
+                }`}
+                id="bracket-links"
+              >
+                {links.map((points, i) => (
+                  <polyline
+                    key={`link-${i}`}
+                    className="fill-none stroke-[62.5] stroke-neutral-300"
+                    points={points}
+                    fill="none"
+                  />
+                ))}
+              </svg>
             </div>
-            <svg
-              className="absolute z-0 overflow-hidden w-full h-full"
-              x={0}
-              y={0}
-              viewBox={`0 0 ${bracket.length * 14000} ${
-                bracket[0].length * 5000
-              }`}
-              id="bracket-links"
-            >
-              {links.map((points, i) => (
-                <polyline
-                  key={`link-${i}`}
-                  className="fill-none stroke-[62.5] stroke-neutral-300"
-                  points={points}
-                  fill="none"
-                />
-              ))}
-            </svg>
           </div>
         </div>
       </div>
@@ -140,15 +171,13 @@ export default async function StageResult({ params }: Params) {
   );
 }
 
-function createBracket(stage: Stage | null) {
+export function createBracket(stage: Stage | null, teams: any) {
   // @ts-ignore
   const size = stage.settings.size;
   const rounds = Math.log2(size);
   const bracket = [];
 
-  let offsets = [1.375, 3.6875, 8.3125, 17.5625, 36.0625];
   let offset = 1.375;
-  // let offset = 1.85;
   for (let i = 0; i < rounds; i++) {
     const round = [];
     const matches = Math.pow(2, rounds - i - 1);
@@ -163,8 +192,15 @@ function createBracket(stage: Stage | null) {
         name = `M.${i + 1}.${j + 1}`;
       }
       const nextMatchIndex = Math.floor(j / 2);
+
+      let match: string[] = [];
+      if (i === 0) {
+        // match = [teams[j].name, teams[teams.length - 1 - j].name];
+        match = [teams[j], teams[teams.length - 1 - j]];
+      }
+
       round.push({
-        match: [],
+        match,
         winner: null,
         name,
         nextMatchIndex,
@@ -182,7 +218,7 @@ function createBracket(stage: Stage | null) {
   return bracket;
 }
 
-function createLinks(bracket: any) {
+export function createLinks(bracket: any) {
   const links = [];
   const matchWidth = 14;
 
