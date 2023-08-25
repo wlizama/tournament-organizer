2eecd0a6372e75c64226c73a23665ba0e43efe8c
bouahth2018 <bouahth2018@gmail.com>
2023-08-25 21:40:57 +0700

    Added match list

--- app/tournaments/[tournamentId]/matches/page.tsx
+++ app/tournaments/[tournamentId]/matches/page.tsx
@@ -6,6 +6,17 @@ async function getMatches(id: string) {
     where: {
       tournamentId: id,
     },
+    include: {
+      stage: true,
+      group: true,
+      round: true,
+    },
+    orderBy: [
+      { stage: { number: "asc" } },
+      { group: { number: "asc" } },
+      { round: { number: "asc" } },
+      { number: "asc" },
+    ],
   });
 }
 
@@ -28,7 +39,7 @@ export default async function Matches({ params }: Params) {
   const stageData = getStage(params.tournamentId);
 
   const [matches, stages] = await Promise.all([matchData, stageData]);
-  // console.log(matches);
+  // console.log(matches[0].opponents);
   // console.log(stages);
 
   return (
@@ -55,6 +66,61 @@ export default async function Matches({ params }: Params) {
             </Link>
           </div>
         ))}
+      </div>
+
+      <div className="mt-8 ring-1 ring-gray-300 sm:mx-0 rounded bg-white">
+        <div className="min-w-full divide-y divide-gray-300">
+          <h1 className="py-5 pl-4 pr-3 text-left text-2xl sm:pl-6">
+            List of Matches
+          </h1>
+          <ul
+            role="list"
+            className="p-4 divide-y divide-gray-100 overflow-hidden"
+          >
+            {matches.map((match) => (
+              <li
+                key={match.id}
+                className="relative flex items-center rounded gap-x-6 p-5 hover:bg-gray-100 sm:px-6"
+              >
+                {/* Match number */}
+                <div className="flex flex-col justify-center w-1/3 text-start">
+                  <div className="">
+                    Match #{match.stage.number}.{match.group.number}.
+                    {match.round.number}.{match.number}
+                  </div>
+                  <div className="text-xs text-neutral-500">
+                    {match.stage.name} - Round {match.round.number}
+                  </div>
+                </div>
+                {/* Opponent names */}
+                <div className="grid flex-1 row-auto justify-stretch gap-[1px]">
+                  <div className="flex flex-nowrap items-center p-0">
+                    <div className="text-ellipsis overflow-hidden whitespace-nowrap flex-[3_1_0%] text-sm">
+                      {match.opponents && match.opponents.length > 0 ? (
+                        (match.opponents[0] as any).participant?.name
+                      ) : (
+                        <div className="text-neutral-300">To be determined</div>
+                      )}
+                    </div>
+                  </div>
+                  <div className="flex flex-nowrap items-center p-0">
+                    <div className="text-ellipsis overflow-hidden whitespace-nowrap flex-[3_1_0%] text-sm">
+                      {match.opponents && match.opponents.length > 0 ? (
+                        (match.opponents[1] as any).participant?.name
+                      ) : (
+                        <div className="text-neutral-300">To be determined</div>
+                      )}
+                    </div>
+                  </div>
+                </div>
+                {/* Match status */}
+                <div className="flex flex-col justify-center w-20 text-xs text-center">
+                  To be played
+                </div>
+              </li>
+            ))}
+          </ul>
+        </div>
       </div>
     </div>
   );
