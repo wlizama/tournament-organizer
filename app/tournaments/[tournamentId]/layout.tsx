e474f354a70fb44d6c281fa241979db17b1c13dd
bouahth2018 <bouahth2018@gmail.com>
2023-08-31 20:36:15 +0700

    Update layout.tsx

--- app/tournaments/[tournamentId]/layout.tsx
+++ app/tournaments/[tournamentId]/layout.tsx
@@ -42,7 +42,7 @@ export default async function TournamentLayout({
           }}
         />
 
-        <main className="absolute xl:pl-72 top-16 xl:top-0 right-0 bottom-0 left-0">
+        <main className="xl:pl-72 top-16 xl:top-0 right-0 bottom-0 left-0">
           <div className="h-full px-4 pb-4 sm:px-6 sm:pb-6 xl:px-10 xl:pb-10 overflow-y-scroll overflow-x-auto">
             {children}
           </div>
