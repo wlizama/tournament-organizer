05b74a480c0c056528ae9abbc899b5119488bc34
Wilder H. Lizama Garcia <wilderlizama@gmail.com>
2023-10-02 16:26:17 -0500

    remove sort from disciplines

--- components/discipline-combo-box.tsx
+++ components/discipline-combo-box.tsx
@@ -23,18 +23,18 @@ export default function DisciplineComboBox({
     platforms([]);
   }
 
-  disciplines.sort((a: any, b: any) => {
-    const nameA = a.name.toUpperCase(); // ignore upper and lowercase
-    const nameB = b.name.toUpperCase(); // ignore upper and lowercase
-    if (nameA < nameB) {
-      return -1;
-    }
-    if (nameA > nameB) {
-      return 1;
-    }
-    // names must be equal
-    return 0;
-  });
+  // disciplines.sort((a: any, b: any) => {
+  //   const nameA = a.name.toUpperCase(); // ignore upper and lowercase
+  //   const nameB = b.name.toUpperCase(); // ignore upper and lowercase
+  //   if (nameA < nameB) {
+  //     return -1;
+  //   }
+  //   if (nameA > nameB) {
+  //     return 1;
+  //   }
+  //   // names must be equal
+  //   return 0;
+  // });
 
   const filteredDiscipline =
     query === ""
